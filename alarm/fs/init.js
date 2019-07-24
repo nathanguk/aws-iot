ebc9d14c53719df469a2dbda012a2c2f7056d97a
Nathan Gaskill <nathang@ANSGRP-002830.local>
2019-07-24 21:48:08 +0100

    Initial commit

---
+++ alarm/fs/init.js
@@ -0,0 +1,61 @@
+// Load Mongoose OS API
+load('api_gpio.js');
+load('api_mqtt.js');
+load('api_sys.js');
+load('api_config.js');
+load('api_timer.js');
+load('api_adc.js');
+load('api_aws.js');
+load('api_events.js');
+load('api_gcp.js');
+load('api_shadow.js');
+
+
+let state = "inactive";
+let deviceid = Cfg.get('device.id');
+
+ADC.enable(36);
+Timer.set(1000, true, function() {
+  
+  let date = Timer.now();
+  //print(date);
+
+  let voltage = ADC.read(36);
+  voltage = (voltage * (5.0 / 1023.0));
+  //print('Voltage:', voltage, "v");
+
+  //Change State to Active
+  if(voltage > 0 && state === "inactive"){
+    state = "active";
+    let topic = deviceid + '/alarm_state';
+    let message = JSON.stringify({
+      device_id: deviceid,
+      voltage: voltage,
+      state: state,
+      date: date
+    });
+    // print(message);
+    // print(state);
+    // print(topic);
+    let ok = MQTT.pub(topic, message, 1);
+    print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
+  };
+
+  //Change State to Inactive
+  if(voltage === 0 && state === "active"){
+    state = "inactive"
+    let topic = deviceid + '/alarm_state';
+    let message = JSON.stringify({
+      device_id: deviceid,
+      voltage: voltage,
+      state: state,
+      date: date
+    });
+    // print(message);
+    // print(state);
+    // print(topic);
+    let ok = MQTT.pub(topic, message, 1);
+    print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
+  };
+  
+}, null);
